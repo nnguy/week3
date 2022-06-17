e6bd8581ca9efc2af7e98f6e69a2d5652756cc50
NELSON NGUY <nelson.nguy001@stu.bmcc.cuny.edu>
2022-06-16 21:03:30 -0400

    Tested how rand works

--- week3/cards.cpp
+++ week3/cards.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <cstdlib> //for rand and srand
+#include <ctime> //for using the time function
 using namespace std; 
 
 int main() {
+	unsigned seed = time(0); 
+	cout << time(0) << endl; 
+
+	srand(seed); 
+	for (int i = 0; i < 52; i++) {
+		cout << rand() % 13 + 1 << endl; 
+	}
 	return 0;
 }

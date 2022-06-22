17df3241a7399c9e1326d9a9696fd9bd37cc1e3e
NELSON NGUY <nelson.nguy001@stu.bmcc.cuny.edu>
2022-06-21 21:04:35 -0400

    added more functions

--- week3/triangle.cpp
+++ week3/triangle.cpp
@@ -2,15 +2,22 @@
 using namespace std; 
 
 double triangle_perimeter(double, double, double); 
+double average(int, int, int); 
 
 int main() {
 	double x, y, z; 
 	cout << triangle_perimeter(1.0, 2.0, 3.0) << endl; 
 	cin >> x >> y >> z; 
-	cout << triangle_perimeter(x, y, z); 
-	return 0; 
+	cout << triangle_perimeter(x, y, z) << endl;
+	double avg = average(x, y, z);
+	cout << avg << endl; 
+	return 0;
 }
 
 double triangle_perimeter(double a, double b, double c) {
 	return a + b + c; 
+}
+
+double average(int a, int b, int c) {
+	return (a + b + c) / static_cast<double>(3); 
 }
